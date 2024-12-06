62e3ab5426a52c2de3bac34bc50879f06362583b
Enwawa <lunglung.id@gmail.com>
2024-12-06 10:48:39 +0700

    update

--- Minggu8/C++/fungsiBV1.cpp
+++ Minggu8/C++/fungsiBV1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int jumlah(int a, int b) {
-    return a + b;
+    return a - b;
 }
 
 int main() {
     int hasil = jumlah(2, 3);
-    cout << "Hasil penjumlahan: " << hasil << endl;
+    cout << "Hasil pengurangan: " << hasil << endl;
     return 0;
 }
