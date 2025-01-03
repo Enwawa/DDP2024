05d2818e7dd5eb7c0d403f9f724d61f893e63ec1
Enwawa <lunglung.id@gmail.com>
2025-01-03 11:40:25 +0700

    update

--- TugasAkhir/Tugasakhir.cpp
+++ TugasAkhir/Tugasakhir.cpp
@@ -6,13 +6,21 @@ using namespace std;
 
 const int MAKS_BARANG = 100;
 
-struct Barang {
+struct Barang { // enwa
     string nama;
     int jumlah;
     double harga;
 };
 
-void cetakStruk(Barang keranjang[], int jumlahBarang, double total, double uang, double kembalian) {
+void tampilkanMenu() { // enwa
+    cout << "1. Tambah Barang\n";
+    cout << "2. Tampilkan Keranjang\n";
+    cout << "3. Pembayaran\n";
+    cout << "4. Keluar\n";
+    cout << "Pilih opsi: ";
+}
+
+void cetakStruk(Barang keranjang[], int jumlahBarang, double total, double uang, double kembalian) { //dedy
     ofstream file("struk_belanja.txt");
     if (file.is_open()) {
         file << "========== STRUK BELANJA ==========" << endl;
@@ -36,15 +44,7 @@ void cetakStruk(Barang keranjang[], int jumlahBarang, double total, double uang,
     }
 }
 
-void tampilkanMenu() {
-    cout << "1. Tambah Barang\n";
-    cout << "2. Tampilkan Keranjang\n";
-    cout << "3. Pembayaran\n";
-    cout << "4. Keluar\n";
-    cout << "Pilih opsi: ";
-}
-
-void tampilkanKeranjang(Barang keranjang[], int jumlahBarang) {
+void tampilkanKeranjang(Barang keranjang[], int jumlahBarang) { //dedy
     if (jumlahBarang == 0) {
         cout << "Keranjang kosong.\n";
         return;
@@ -60,7 +60,7 @@ void tampilkanKeranjang(Barang keranjang[], int jumlahBarang) {
     }
 }
 
-void pembayaran(Barang keranjang[], int jumlahBarang) {
+void pembayaran(Barang keranjang[], int jumlahBarang) { //fatih
     if (jumlahBarang == 0) {
         cout << "Keranjang kosong. Tidak ada yang perlu dibayar.\n";
         return;
@@ -80,7 +80,7 @@ void pembayaran(Barang keranjang[], int jumlahBarang) {
     cetakStruk(keranjang, jumlahBarang, total, uang, kembalian);
 }
 
-int main() {
+int main() {  // enwa
     Barang keranjang[MAKS_BARANG];
     int jumlahBarang = 0, pilihan;
 
